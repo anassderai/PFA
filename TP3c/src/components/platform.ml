3f8be57c2ce1fb1dfdca26378db62cc63a4b609c
Keryan C <cossard.keryan@gmail.com>
2025-03-31 13:38:41 +0200

    update platform

--- TP3c/src/components/platform.ml
+++ TP3c/src/components/platform.ml
@@ -7,7 +7,7 @@ let create x y w h  =
   let p = new platform in
   p#pos#set Vector.{ x = float x; y = float y };
   p#rect#set Rect.{width = w; height = h};
-  p#texture#set Texture.transparent;
+  p#texture#set Texture.blue;
   p#mass#set Float.infinity;
   p#index#set 7;
   Draw_system.register (p :> drawable);
@@ -19,21 +19,25 @@ let create x y w h  =
 let platform_1 () =
   platform := Array.map (fun x y w h -> create x y w h)
     [|
-      56; 320;  48; 8;
-      64; 272; 40; 8;
-      48;372;  52; 8;
-      176;400; 44;8;
-      172;296; 32;4;
-      216; 344;56;8; 
-      232; 448;  40; 8;
-      176;500;  44;8;
+      56;  320; 48; 8;
+      64;  272; 40; 8;
+      48;  372; 52; 8;
+      176; 400; 44; 8;
+      172; 296; 32; 4;
+      216; 344; 56; 8; 
+      232; 448; 40; 8;
+      176; 500; 44; 8;
       328; 328; 56; 8;
       480; 308; 56; 8;
-      532;252;  68;4;
+      532; 252; 68; 4;
       636; 200; 52; 8;
-      740; 160; 60;8;
+      740; 160; 60; 8;
     |]
 
-
-
-(* (736; 96; Texture.red; 64;64)(*portail*)*)
+let unregister () = 
+  Array.iter (fun p -> 
+    Draw_system.unregister (p :> drawable);
+    Collision_system.unregister (p :> collidable);
+    Force_system.unregister (p :> physics);
+  ) !platform;
+  platform := [||]
